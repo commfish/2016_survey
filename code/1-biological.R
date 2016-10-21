189aa324c66bf4160876b8c7abae5207edb951ec
Ben Williams <ben.williams@alaska.gov>
2016-10-20 17:40:45 -0800

    initial commit

---
+++ code/1-biological.R
@@ -0,0 +1,28 @@
+# load ----
+library(tidyverse)
+theme_set(theme_bw())
+# data ----
+#clean up event data- change names and data types etc.
+events <- read.csv('./data/events_2016.csv')
+events %>% mutate(ID = gsub(" ", "", STATION_ID),date = as.Date(DATE_SET, format='%m-%d-%Y'),
+                  year = as.numeric(format(date, "%Y"))) %>% 
+   select(year=year, date=date, Event = EVENT_ID, District = DISTRICT, 
+          Dredge = DREDGE_ID, Bed = BED_SW, Type = STATION_TYPE, ID = ID, 
+          slat = START_LATITUDE, slon=START_LONGITUDE, sdepth = DEPTH_START_F, 
+          stime = START_TIME, speed=TOW_SPEED, maxdepth=Max_Dpth_fa, mindepth=Min_Dpth_fa, 
+          elat=END_LATITUDE, elon=END_LONGITUDE, edepth=DEPTH_END_F, etime=END_TIME, 
+          calc_length=TOW_LENGTH_CALC, field_length=TOW_LENGTH_FIELD,length=TOW_LENGTH_DESIGNATED,
+          performance=GEAR_PERFORMANCE_CODE_SW) -> event
+
+catch <- read.csv('./data/catchComp_2016.csv')
+catch %>% select(Event = EVENT_ID, species=RACE_CODE, 
+                 size_class=SCAL_SIZE_CLASS, count=COUNT,
+                 sample_wt=SAMPLE_WT_KG, cond = CONDITION_CODE_RII, 
+                 sample_type = SAMPLE_TYPE) -> catch
+
+awl <- read.csv('./data/awl_2016.csv')
+awl %>% select(Event = EVENT_ID, weight=WHOLE_WT_GRAMS, 
+               worm=SHELL_WORM_SW, height=SHELL_HEIGHT_MM, sex=SEX_SW, 
+               gonad_cond=SCAL_GONAD_COND, blister=MUD_BLISTER_SW, 
+               meat_cond=MEAT_CONDITION_SW, clapper = CLAPPER, 
+               sample_type = SAMPLE_TYPE) -> awl

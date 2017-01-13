c4b37cbc6fee233bd00d51ffabb7e2d27e8c7f9d
Joshua Mumm <joshua.mumm@alaska.gov>
2017-01-13 10:37:36 -0900

    Limited sums to only unsorted catch.  Combined steps.

--- code/CPUE.R
+++ code/CPUE.R
@@ -1,14 +1,13 @@
-#  ###################
+## ###################
 #    CPUE           ##
 ######################
-#Josh Mumm  170112
-#Calc CPUE (cnt/nmi). Tanners only for now.  Could expand to other species. 
-#May combine steps. 
+#Josh Mumm  170113
+#Calc CPUE (cnt/nmi). Tanners only for now, could expand to other species. 
 
 library (tidyverse)
 
-## LOAD DATA #############################################################################
-#clean up event data- change names and data types etc.
+## LOAD DATA ##########################################################################
+
 events <- read.csv('./data/events_2016_161027.csv')
 events %>% filter (GEAR_PERFORMANCE_CODE_SW == 1, STATION_TYPE != "Ancillary") %>%
             select( Event = EVENT_ID,
@@ -22,37 +21,27 @@ catch %>% select(Event = EVENT_ID, species=RACE_CODE,
                  sample_wt=SAMPLE_WT_KG, cond = CONDITION_CODE_RII, 
                  sample_type = SAMPLE_TYPE) %>% filter(Event %in% event$Event) -> catch
 
-## ESTIMATE CATCH BY TOW, then CPUE, and aggregate by Bed #################################
+## ESTIMATE CATCH BY TOW, then CPUE, and aggregate by Bed ##############################
 
-# sum weights of all species by sample type and tow
-catch %>% group_by(Event) %>% summarise(
-    t1 = sum(sample_wt[sample_type == 1]),
-    t2 = sum(sample_wt[sample_type == 2])) %>%
-    right_join (event) %>% mutate (t1 = replace(t1, which(is.na(t1)), 0),  # join events with no catch and set NA to 0. 
-                                   t2 = replace(t2, which(is.na(t2)), 0)) -> totWt
-
-# sum counts of species of interest by sample type tow
 s <- 68560 # Tanners for now
-catch %>% filter(species == s) %>% group_by(Event) %>% 
-  summarise (s1 = sum(count[sample_type == 1]), 
-             s2 = sum(count[sample_type == 2])) ->  spCnt
 
-#join total wts to species count and set NAs to 0
-left_join(totWt, spCnt) %>% mutate (s1 = replace(s1, which(is.na(s1)), 0),
-                                    s2 = replace(s2, which(is.na(s2)), 0)) -> comb
-  
-# expand any T2's, sum components of catch, and calc CPUE   
-comb %>% mutate ( sTot = s1 + s2 + if_else(t2>0, (t1*s2/t2), 0), # catch of species s by event.  Con to prevent div by 0.
-                  sCPM = sTot/length                    # CPUE cnt/nmi 
-                  ) -> c 
-
-# aggregate CPUE by bed
-c %>% group_by(Bed) %>% summarise ( mean = mean(sCPM), 
-                                    sd = sqrt(var(sCPM)), 
-                                    cv = 100 * sd/mean) -> cpm
-cpm   
-
-                  
+# aggregate unsorted catch and species interest 
+catch %>% group_by(Event) %>% 
+    summarise(t1 = sum(sample_wt[species == 99997 & sample_type == 1]),
+              t2 = sum(sample_wt[species == 99997 & sample_type == 2]),
+              s1 = sum(count[species == s & sample_type == 1]), 
+              s2 = sum(count[species == s & sample_type == 2])) %>% 
+  # join to event for Bed and set any events with no catch to 0 (none in 2016). 
+      right_join (event) %>% replace_na (list(t1 = 0, t2 = 0, s1 = 0, s2 = 0)) %>%
+  # expand any T2's, sum components of catch, and calc CPUE   
+      mutate ( sTot = s1 + s2 + if_else(t2>0, (t1*s2/t2), 0), #expanded catch of species s by event.  Con to prevent div by 0.
+               sCPM = sTot/length)  %>%                    # CPUE cnt/nmi 
+  # aggregate by Bed     
+      group_by(Bed) %>% summarise ( cpue_mean = mean(sCPM), 
+                                    cpue_sd = sqrt(var(sCPM)), 
+                                    cpue_cv = 100 * cpue_sd/cpue_mean,
+                                    totCatch = sum(sTot)) -> cpm
+cpm # CPUE (cnt/nmi) with sd, cv and total expanded catch (cnt) by bed, all for sp. s.     
 
 
 

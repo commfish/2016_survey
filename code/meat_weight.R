f332cd1a3c1a477755091a51fe2747f0c368f865
Katie Palof <katie.palof@alaska.gov>
2017-01-23 15:50:31 -0900

    work to do ks test by event

--- code/meat_weight.R
+++ code/meat_weight.R
@@ -86,6 +86,51 @@ awl %>% select(Event = EVENT_ID,  species=RACE_CODE,
 as.data.frame(do.call(rbind,scal.catch$dat)) %>% filter(variable=='large') %>% 
   left_join(samp) %>% filter(ratio>0)-> meat.wts
 
+#awl 10 represenative of 40? ------------
+awl %>% select(Event = EVENT_ID,  species=RACE_CODE,
+                   j = SCALLOP_NUMBER, size_class = SCAL_SIZE_CLASS,
+                   weight=WHOLE_WT_GRAMS, worm=SHELL_WORM_SW, 
+                   height=SHELL_HEIGHT_MM, meat_weight = MEAT_WEIGHT_GRAMS,
+                   clapper = CLAPPER, sample_type = SAMPLE_TYPE) %>% 
+  filter(species == 74120, size_class == 1, is.na(clapper), !is.na(height), 
+         Event %in% event$Event ) %>% mutate(m_weight = ifelse(!is.na(weight), "mw", 'ht'))%>%
+  select(Event, j, height, weight, meat_weight, m_weight) ->samp2
+
+
+#sample size for each event - n
+samp2 %>% group_by(Event) %>% mutate(maxj = max(j), n = n()) %>% select(Event, maxj,n)-> samp2_ssize
+# only include Event is n is > 11
+samp2_ssize %>% 
+  group_by(Event) %>% summarise(n =mean(n)) %>% 
+  filter(n >11) -> samp2_ssize2 # Events with large enough samples sizes for kstest
+
+# for each event does m_weight group 1 represent heights in m_weight group 2 ?
+samp2 %>% filter(Event %in% samp2_ssize2$Event) -> samp3
+
+samp3 %>% 
+  group_by(Event) %>% do(dat=(.)) %>% select(dat) %>% map(identity) -> samp3.list # list for each event
+#need to filter for those events that don't have both...
+
+#K-S test
+ks_height <- do.call(rbind, lapply(samp3.list$dat, ks_func))
+
+samp2 %>% filter(Event == '2016D02003') -> test1
+test1 %>% spread(m_weight, height, fill =NA) -> test1a
+hat <- ks.test(test1a$ht, test1a$mw)
+out = c(test1a$Event[1], hat$p.value)
+ks_func(test1)
+
+ks_func <- function(x){
+  #first turn the list into a dataframe
+  #use dplyr to seperate into two groups y and z to compare
+  # output is event id and p-value
+  x = as.data.frame(x)
+  x %>% spread(m_weight, height, fill=NA) ->inter
+  ks <-ks.test(inter$ht, inter$mw)
+  out <- cbind(inter$Event[1], ks$p.value)
+  out
+}
+
 #turn meat weights into list for analysis
 meat.wts %>% 
   group_by(Bed) %>% 

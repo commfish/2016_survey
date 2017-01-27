410acc3bf79d5341b484595c0dfbce3ab0330696
Katie Palof <katie.palof@alaska.gov>
2017-01-27 08:34:06 -0900

    added function to calculate density of clappers per bed

--- code/functions.R
+++ code/functions.R
@@ -75,3 +75,26 @@ f.wt <- function(x){
     bind_rows %>% 
     mutate(replicate=1:n())
 }
+
+
+#Clapper density summerization function --------------------
+f.clap <- function(x){
+  # first turn the list to a dataframe
+  # use dplyr to summarise each list
+  # output is one row all stats.
+  
+  x = as.data.frame(x)
+  x %>%
+    group_by(year, District, Bed)%>%
+    summarise(n=mean(n),
+              area = mean(area_nm2) ,
+              dbar = (1/n*sum(di)),
+              var_dbar=1/((n)-1)*sum((di-dbar)^2) ,
+              cv=sqrt(var_dbar)/dbar*100,
+              ss=sum((di-dbar)^2),
+              N=area*dbar,
+              varN=(area^2)*1/n*1/(n-1)*ss,
+              cvN=sqrt(varN)/N*100) -> out
+  out
+}
+

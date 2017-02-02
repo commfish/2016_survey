06a7dfb9db1d00ea60f3039e520f8e36652e0967
Ben Williams <ben.williams@alaska.gov>
2017-02-01 18:19:23 -0900

    updates to some of josh's code

--- code/biological.R
+++ code/biological.R
@@ -3,6 +3,10 @@ library(tidyverse)
 theme_set(theme_bw()+ 
             theme(panel.grid.major = element_blank(),
                   panel.grid.minor = element_blank()))
+library (xtable)
+library (knitr)
+options (scipen = 999)
+
 # data ----
 #clean up event data- change names and data types etc.
 events <- read.csv('./data/events_2016_161027.csv')
@@ -14,7 +18,7 @@ events %>% mutate(ID = gsub(" ", "", STATION_ID),date = as.Date(DATE_SET, format
          stime = START_TIME, speed=TOW_SPEED, maxdepth=Max_Dpth_fa, mindepth=Min_Dpth_fa, 
          elat=END_LATITUDE, elon=END_LONGITUDE, edepth=DEPTH_END_F, etime=END_TIME, 
          calc_length=TOW_LENGTH_CALC, field_length=TOW_LENGTH_FIELD,length=TOW_LENGTH_DESIGNATED,
-         performance=GEAR_PERFORMANCE_CODE_SW) %>% 
+         performance=GEAR_PERFORMANCE_CODE_SW, Vessel=VESSEL_NAME) %>% 
   filter (performance == 1, Type != "Ancillary") -> event
 
 catch <- read.csv('./data/catchComp_2016_161027.csv')
@@ -29,13 +33,15 @@ awl %>% select(Event = EVENT_ID, species=RACE_CODE, weight=WHOLE_WT_GRAMS,
                worm=SHELL_WORM_SW, height=SHELL_HEIGHT_MM, sex=SEX_SW, 
                gonad_cond=SCAL_GONAD_COND, blister=MUD_BLISTER_SW, 
                meat_cond=MEAT_CONDITION_SW, clapper = CLAPPER, 
-               sample_type = SAMPLE_TYPE) %>%
+               sample_type = SAMPLE_TYPE, meat_wt = MEAT_WEIGHT_GRAMS) %>%
   filter (Event %in% event$Event) -> awl
 
 # height ----
 #height/weight relationship
-awl %>% left_join(event) %>% 
-  ggplot(aes(weight,height))+geom_point()+facet_wrap(~Bed)+stat_smooth() 
+awl %>% left_join(event) %>% filter(Bed=='KSH1') %>% 
+   ggplot(aes(height, fill=Vessel))+geom_density(alpha=.2)
+   
+
 
 #ggsave("./figs/Fig1.tiff", dpi=300, height=6, width=6, units="in")
 
@@ -44,14 +50,11 @@ awl %>% left_join(event) %>%
   ggplot(aes(height))+geom_density(alpha=.2,fill=4)+facet_wrap(~Bed)
 
 #height densities - 
-awl %>% left_join(event) %>% 
+awl %>% left_join(event) %>% filter(Bed=='KSH1', sex!='NA') %>% 
   ggplot(aes(height, fill=factor(sex),color=factor(sex)))+geom_density(alpha=.2)+facet_wrap(~Bed)
 # getting some issues between using 0 and using NA - need to examine this.
 
 # OTHER SCAL BIOLOGICAL DATA TABLES ----
-library (xtable)
-library (knitr)
-options (scipen = 999)
 
 awl %>% filter(species == 74120, is.na(clapper)) %>% left_join (event) -> bio # not adding events with no scals
 
@@ -74,16 +77,22 @@ blist.pn <- rbind(blist.p, margin.table(blist, 2))
 weak.pn <-  rbind(weak.p, margin.table(weak, 2))
 
 # change row names.  Should use LUTs or some other type of substitution, in case classes observed in future changes. 
-rownames(worm.pn) <- c("0%", "1 - 24%", "25 - 49%", "50 - 74%", "75 - 100%", "n")
-rownames(gonad.pn) <- c("Immature", "Empty", "Initial Recovery", "Filling", "Full", "Cannot Determine", "n")
-rownames(blist.pn) <-c("0%", "1 - 24%", "25 - 49%", "50 - 74%", "n")   # no 4's (75-100%) this year
-rownames(weak.pn) <- c("Good", "Weak", "n")
+worm.pn <- as.data.frame(worm.pn) %>% mutate(Percent=c("0%", "1 - 24%", "25 - 49%", "50 - 74%", "75 - 100%", "n"))
+gonad.pn <- as.data.frame(gonad.pn) %>% mutate(Stage=c("Immature", "Empty", "Initial Recovery", "Filling", "Full", "Cannot Determine", "n"))
+blist.pn <- as.data.frame(blist.pn) %>% mutate(Percent = c("0%", "1 - 24%", "25 - 49%", "50 - 74%", "n"))   # no 4's (75-100%) this year
+weak.pn <- as.data.frame(weak.pn) %>% mutate(Meats = c("Good", "Weak", "n"))
 
 # reorder kayak beds together
-worm.pn <- worm.pn[,c("EK1","WK1","KSH1", "KSH2", "KSH3")]
-gonad.pn <- gonad.pn[,c("EK1","WK1","KSH1", "KSH2", "KSH3")]
-blist.pn <- blist.pn[,c("EK1","WK1","KSH1", "KSH2", "KSH3")]
-weak.pn <- weak.pn[,c("EK1","WK1","KSH1", "KSH2", "KSH3")]
+worm.pn <- worm.pn[,c("Percent","EK1","WK1","KSH1", "KSH2", "KSH3")]
+gonad.pn <- gonad.pn[,c("Stage","EK1","WK1","KSH1", "KSH2", "KSH3")]
+blist.pn <- blist.pn[,c("Percent","EK1","WK1","KSH1", "KSH2", "KSH3")]
+weak.pn <- weak.pn[,c('Meats',"EK1","WK1","KSH1", "KSH2", "KSH3")]
+
+
+write_csv(worm.pn,'./output/worm.csv')
+write_csv(gonad.pn,'./output/gonad.csv')
+write_csv(blist.pn,'./output/blist.csv')
+write_csv(weak.pn,'./output/weak.csv')
 
 # cumbersome code to limit number of digits row-wise. Bottom row is n, others are %. 
 worm.dig <- matrix  (c(rep(2,nrow(worm.pn) - 1),0),  nrow=nrow(worm.pn),  ncol=ncol(worm.pn) + 1) 
